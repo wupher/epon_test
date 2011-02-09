e662a19e655fdb1744167a0f99828b24c3e401a3
Wupher Fan <wupher.fan@gmail.com>
2011-02-10 00:09:21 +0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EponTest::Application.config.secret_token = '89d64ffc2a86e1e5842de65ee01d576d65c5037e40f81557c57755f1c4fdcd9a7b8a110368cb7b87ff6b7d0a58034873506e44c2fca0ca5a8db42a59cfa3d0d9'
